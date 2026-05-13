993ccf3f948bb60a475bcdeedb366ffc64b230df
pal4dinX <tobizueira549@gmail.com>
2026-05-12 21:20:30 -0300

    css melhorado

--- script.js
+++ script.js
@@ -7,12 +7,17 @@ function toggleMenu() {
   }
   
   function showSection(id) {
-    const sections = document.querySelectorAll("section");
-  
-    sections.forEach(sec => sec.classList.remove("active"));
-  
-    document.getElementById(id).classList.add("active");
-  
-    document.getElementById("sidebar").classList.remove("active");
-    document.querySelector(".menu-btn").classList.remove("active");
-  }
+  const sections = document.querySelectorAll("section");
+  sections.forEach(sec => sec.classList.remove("active"));
+
+  const target = document.getElementById(id);
+  target.classList.add("active");
+
+  // reinicia a animação
+  target.style.animation = "none";
+  target.offsetHeight; // força o reset
+  target.style.animation = "";
+
+  document.getElementById("sidebar").classList.remove("active");
+  document.querySelector(".menu-btn").classList.remove("active");
+}
